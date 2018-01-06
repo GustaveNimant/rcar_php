3bf586701c2b66990e4c6dba1a3a6d307812e2d1
Collectif Willforge <rcar@willforge.fr>
2018-01-06 13:52:11 +0100

    Works. Redo works. needs full check

--- block_new_create_script.php
+++ block_new_create_script.php
@@ -13,6 +13,11 @@ entering_in_script ($script);
 
 father_n_son_stack_script_push_of_current_script ($script);
 
+# print_html_array ($script, '$_SESSION["irp_register"]', $_SESSION['irp_register']);
+
+$get_key = 'block_new_surname';
+irp_path_clean_register_of_top_key_of_bottom_key_of_where ('block_new_content_write', 'GET_' . $get_key, $script);
+
 $html_str = irp_provide ($entity, $script);
 
 print $html_str;

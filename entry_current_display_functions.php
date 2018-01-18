06b646ea4bc616bf6eb9a988bccee5604e28bf2f
François Colonna <f.colonnacesari@free.fr>
2018-01-18 18:52:33 +0100

    Works entry_current_rename cleaned

--- entry_current_display_functions.php
+++ entry_current_display_functions.php
@@ -59,7 +59,7 @@ function entry_current_display_build (){
     $html_str .= irp_provide ('toward_block_new_create_form', $here);
     $html_str .= '<br>' . "\n";
 
-    $html_str .= irp_provide ('toward_entry_current_rename', $here);
+    $html_str .= irp_provide ('entry_current_rename_form', $here);
     $html_str .= '<br><br>' . "\n";
 
     if (file_directory_is_not_empty_of_directory_path ($fnd_ent_cur)) {/* Improve */
--- entry_current_rename_form_functions.php
+++ entry_current_rename_form_functions.php
@@ -8,12 +8,17 @@ $Documentation[$module]['what for'] = "to ...";
 
 entering_in_module ($module);
 
-function toward_entry_current_rename_title_build () {
+function entry_current_rename_form_title_build () {
   $here = __FUNCTION__;
   entering_in_function ($here);
 
+  $sur_ent_cur = irp_provide ('entry_current_surname_from_entry_current_name', $here);
+ 
   $en_bub_tit = 'rename the entry';
-  $la_bub_Tit = bubble_bubbled_capitalized_la_text_of_en_text ($en_bub_tit);
+
+  $la_bub_tit  = bubble_bubbled_la_text_of_en_text ($en_bub_tit);
+  $la_bub_Tit  = string_html_capitalized_of_string ($la_bub_tit);
+  $la_bub_Tit .= ' <i><b>' . ucfirst ($sur_ent_cur) . '</b></i> ';
 
   $html_str  = comment_entering_of_function_name ($here); 
   $html_str .= common_html_span_background_color_of_html ($la_bub_Tit);
@@ -25,7 +30,7 @@ function toward_entry_current_rename_title_build () {
   return $html_str;
 }
 
-function toward_entry_current_rename_form_build () {
+function entry_current_rename_form_build () {
   $here = __FUNCTION__;
   entering_in_function ($here);
 
@@ -36,7 +41,7 @@ function toward_entry_current_rename_form_build () {
 
   $html_str  = comment_entering_of_function_name ($here); 
   $html_str .= '<form action="' . $script_action .'" method="get">' . "\n";
-  $html_str .= irp_provide ('toward_entry_current_rename_title', $here);
+  $html_str .= irp_provide ('entry_current_rename_form_title', $here);
   $html_str .= inputtypesubmit_of_en_action_name ('rename');
   $html_str .= '</form>' .  "\n";
   $html_str .= comment_exiting_of_function_name ($here);
@@ -46,21 +51,6 @@ function toward_entry_current_rename_form_build () {
 
   return $html_str;
 };
-
-function toward_entry_current_rename_build () {
-  $here = __FUNCTION__;
-  entering_in_function ($here);
-
-  $html_str  = comment_entering_of_function_name ($here); 
-  $html_str .= irp_provide ('toward_entry_current_rename_form', $here);
-  $html_str .= comment_exiting_of_function_name ($here);
-
-  debug_n_check ($here , '$html_str',  $html_str);
-
-  exiting_from_function ($here);
-
-  return $html_str;
-}
 
 exiting_from_module ($module);
 

b28bdc151489bf88c78402d58957f2ccc912c0b1
François Colonna <f.colonnacesari@free.fr>
2018-03-14 13:55:01 +0100

    Works. Except Undo when renamed History. Cleaned reorder : no more input hidden

--- block_list_order_new_library_old.php
+++ block_list_order_new_library_old.php
@@ -10,7 +10,7 @@ $Documentation[$module]['what for'] = "to ...";
 function block_name_list_order_current_of_surname_by_name_hash_of_block_name_list_order_current ($sur_by_nam_h, $nam_blo_a) { 
     $here = __FUNCTION__;
     entering_in_function ($here);
-    debug_n_check ($here , '$nam_blo_a', $nam_blo_a); 
+    # debug_n_check ($here , '$nam_blo_a', $nam_blo_a); 
     
     $html_str  = comment_entering_of_function_name ($here);
     $html_str .= '<br> ';
@@ -36,29 +36,38 @@ function block_name_list_order_current_of_surname_by_name_hash_of_block_name_lis
     return $html_str;
 }
 
-function swap_of_block_name_array_of_block_name_from_of_block_name_to ($nam_blo_a, $nam_blo_fr, $nam_blo_to) {
-  $here = __FUNCTION__;
-  entering_in_function ($here . " ($nam_blo_fr, $nam_blo_to)");
-  debug_n_check ($here , '$nam_blo_a', $nam_blo_a); 
+function swap_two_blocks ($nam_blo_a) {
+    $here = __FUNCTION__;
+  entering_in_function ($here);
+  # debug_n_check ($here , '$nam_blo_a', $nam_blo_a); 
+
+  $nam_blo_fr = get_hash_retrieve_value_of_get_key_of_where ('from', $here);
+  $nam_blo_to = get_hash_retrieve_value_of_get_key_of_where ('to', $here);
 
   $pro_key_fr = array_retrieve_only_key_of_value_of_array_of_where ($nam_blo_fr, $nam_blo_a, $here);
   $pro_key_to = array_retrieve_only_key_of_value_of_array_of_where ($nam_blo_to , $nam_blo_a, $here);
 
   $mod_pro_a  = array_swap ($pro_key_fr, $pro_key_to, $nam_blo_a);
 
-  debug_n_check ($here , '$mod_pro_a', $mod_pro_a); 
+  # debug_n_check ($here , "output block name array:", $mod_pro_a); 
   exiting_from_function ($here);
 
   return $mod_pro_a;
 }
 
-function after_of_block_name_array_of_block_name_from_of_block_name_to ($nam_blo_a, $nam_blo_fr, $nam_blo_to) {
+function after_one_block ($nam_blo_a) {
   $here = __FUNCTION__;
-  entering_in_function ($here . " ($nam_blo_fr, $nam_blo_to)");
+  entering_in_function ($here);
   debug_n_check ($here , '$nam_blo_a', $nam_blo_a); 
  
+  $nam_blo_fr = get_hash_retrieve_value_of_get_key_of_where ('from', $here);
+  $nam_blo_to = get_hash_retrieve_value_of_get_key_of_where ('to', $here);
+
+  debug_n_check ($here , "block name from:", $nam_blo_fr); 
+  debug_n_check ($here , "block name to:", $nam_blo_to);  
+
   $ren_arr_a = renumber_keys_of_step_of_array (2, $nam_blo_a);
-  debug_n_check ($here , '$ren_arr_a', $ren_arr_a); 
+  # debug_n_check ($here , "renumbered block name array:", $ren_arr_a); 
 
   $pro_key_fr = array_retrieve_only_key_of_value_of_array_of_where ($nam_blo_fr, $ren_arr_a, $here);
   $pro_key_to = array_retrieve_only_key_of_value_of_array_of_where ($nam_blo_to , $ren_arr_a, $here);
@@ -72,23 +81,29 @@ function after_of_block_name_array_of_block_name_from_of_block_name_to ($nam_blo
 
   ksort ($ren_arr_a);
   $ren_arr_a = renumber_keys_of_step_of_array (1, $ren_arr_a);
-
   $log_str .= "Keys of array \$ren_arr_a have been renumbered by ?" . "\n";
-  file_log_write ($here, $log_str);
 
+  file_log_write ($here, $log_str);
   debug_n_check ($here , '$ren_arr_a', $ren_arr_a); 
   exiting_from_function ('exiting :' . $here);
 
   return $ren_arr_a;
 
 }
-function before_of_block_name_array_of_block_name_from_of_block_name_to ($nam_blo_a, $nam_blo_fr, $nam_blo_to) {
+
+function before_one_block ($nam_blo_a) {
   $here = __FUNCTION__;
-  entering_in_function ($here . " ($nam_blo_fr, $nam_blo_to)");
+  entering_in_function ($here);
   debug_n_check ($here , '$nam_blo_a', $nam_blo_a); 
  
+  $nam_blo_fr = get_hash_retrieve_value_of_get_key_of_where ('from', $here);
+  $nam_blo_to = get_hash_retrieve_value_of_get_key_of_where ('to', $here);
+
+  debug_n_check ($here , "block name from:", $nam_blo_fr); 
+  debug_n_check ($here , "block name to:", $nam_blo_to);  
+
   $ren_arr_a = renumber_keys_of_step_of_array (2, $nam_blo_a);
-  debug_n_check ($here , '$ren_arr_a', $ren_arr_a); 
+  # debug_n_check ($here , "renumbered block name array:", $ren_arr_a); 
 
   $pro_key_fr = array_retrieve_only_key_of_value_of_array_of_where ($nam_blo_fr, $ren_arr_a, $here);
   $pro_key_to = array_retrieve_only_key_of_value_of_array_of_where ($nam_blo_to , $ren_arr_a, $here);
@@ -106,30 +121,30 @@ function before_of_block_name_array_of_block_name_from_of_block_name_to ($nam_bl
 
   file_log_write ($here, $log_str);
 
-  debug_n_check ($here , '$ren_arr_a', $ren_arr_a); 
+  debug_n_check ($here , "output block name array:", $ren_arr_a); 
   exiting_from_function ($here);
 
   return $ren_arr_a;
 
 }
 
-function block_current_name_reordered_array_of_en_order_of_block_name_list_order_current_of_block_name_from_of_block_name_to ($en_order, $old_pro_a, $nam_blo_fr, $nam_blo_to) {
+function block_current_name_reordered_array_of_en_order_of_block_name_list_order_current ($en_order, $old_pro_a) {
   $here = __FUNCTION__;
-  entering_in_function ($here . " ($en_order, $nam_blo_fr, $nam_blo_to)");
-  debug_n_check ($here ,'$old_pro_a', $old_pro_a);
+  entering_in_function ($here . " ($en_order, ... array)");
+  # debug_n_check ($here , "old block order array", $old_pro_a);
 
   switch ($en_order) {
   case 'move before':
-      $new_pro_a = before_of_block_name_array_of_block_name_from_of_block_name_to ($old_pro_a, $nam_blo_fr, $nam_blo_to);
-      debug_n_check ($here , 'move before $new_pro_a', $new_pro_a);
+      $new_pro_a = before_one_block ($old_pro_a);
+      # debug_n_check ($here , "new block order array", $new_pro_a);
       break;
   case 'move after':
-      $new_pro_a = after_of_block_name_array_of_block_name_from_of_block_name_to ($old_pro_a, $nam_blo_fr, $nam_blo_to);
-      debug_n_check ($here , 'move after $new_pro_a', $new_pro_a);
+      $new_pro_a = after_one_block ($old_pro_a);
+      # debug_n_check ($here , "new block order array", $new_pro_a);
       break;
   case 'swap':
-      $new_pro_a = swap_of_block_name_array_of_block_name_from_of_block_name_to ($old_pro_a, $nam_blo_fr, $nam_blo_to);
-      debug_n_check ($here , 'swap $new_pro_a', $new_pro_a);
+      $new_pro_a = swap_two_blocks ($old_pro_a);
+      # debug_n_check ($here , "new block order array", $new_pro_a);
       break;
   default:
       print "<br>Fatal Error in $here : case >$order< is unknown. Check";
@@ -139,5 +154,7 @@ function block_current_name_reordered_array_of_en_order_of_block_name_list_order
   exiting_from_function ($here);
   return $new_pro_a;
 }
+
+
 
 ?>
--- block_name_list_order_new_array_functions.php
+++ block_name_list_order_new_array_functions.php
@@ -16,23 +16,17 @@ function block_name_list_order_new_array_build () {
 
   $nam_mod_cur = module_name_of_module_fullnameoffile (__FILE__);
 
-  $nam_blo_ord_cur_a = irp_provide ('block_name_array_order_current', $here);
-
-/* getting DATA $get_val */
-  /* $get_key = 'block_name_list_reorder_action_la';  */
-  /* $la_Order = irp_data_value_retrieve_and_store_of_get_key_of_module_name_of_where ($get_key, $nam_mod_cur, $here); */
-
   $la_Order = irp_provide ('block_name_list_reorder_action_la', $here);
   $la_order = strtolower ($la_Order);
   $en_order = language_translate_to_english_of_la_string ($la_order);
 
-/* ICI 
-  $from = irp_provide ('from', $here);
-  $to = irp_provide ('to', $here);
-  $nam_blo_ord_new_a = block_current_name_reordered_array_of_en_order_of_block_name_list_order_current_of_from_of_o  ($en_order, $nam_blo_ord_cur_a, $from, $to); 
-*/
+  $nam_blo_fr = irp_provide ('from', $here);
+  $nam_blo_to = irp_provide ('to', $here);
 
-  $nam_blo_ord_new_a = block_current_name_reordered_array_of_en_order_of_block_name_list_order_current ($en_order, $nam_blo_ord_cur_a); 
+  $nam_blo_ord_cur_a = irp_provide ('block_name_array_order_current', $here);
+  debug_n_check ($here , '$nam_blo_ord_cur_a', $nam_blo_ord_cur_a);
+
+  $nam_blo_ord_new_a = block_current_name_reordered_array_of_en_order_of_block_name_list_order_current_of_block_name_from_of_block_name_to ($en_order, $nam_blo_ord_cur_a, $nam_blo_fr, $nam_blo_to);
 
   debug_n_check ($here , '$nam_blo_ord_new_a', $nam_blo_ord_new_a);
   exiting_from_function ($here);
